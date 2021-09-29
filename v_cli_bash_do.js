0ec0c91911a959c3eb60249ff17d60765d9ec12c
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-09-29 23:08:21 +0200

    yeaaaaa callbacks

--- v_cli_bash_do.js
+++ v_cli_bash_do.js
@@ -1,16 +1,37 @@
 /*jshint esversion: 6 */
 const { exec } = require('child_process');
+const vdebug = () => {
+  this._value = true;
+
+  this.value = () => {
+    return this.get();
+  };
+  this.get = () => {
+    return (typeof this._value !== 'undefined') ? this._value : null;
+  };
+
+  return this.value();
+};
+
+async function logMessage  (msg) {
+  if (vdebug()) {
+    console.log(msg);
+    return vdebug();
+  }
+  return false;
+};
 
 // This is the actual function that will be called to execute the command
-const bashDo=(inputCommand = '')=>{
+async function bashDo(inputCommand = '', cb = 'undefined'){
     exec(inputCommand, (err, stdout, stderr) => {
       if (err) {
-        //some err occurred
-        console.error(err)
+        logMessage(err);
+        return err;
       } else {
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`)
-        console.log(`stderr: ${stderr}`)
+        var response =  logMessage({ stdout, stderr });
+        
+        (typeof cb === 'function') ? cb(response) : "" ;
+        var helper2 =  response;
       }
     });
   };
