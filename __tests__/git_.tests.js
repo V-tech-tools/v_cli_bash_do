8889a4f2d9f5483fdc866c62f967447f8534a95d
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-16 09:25:13 +0100

    yea changes

--- __tests__/git_.tests.js
+++ __tests__/git_.tests.js
@@ -8,6 +8,11 @@ test('empty exec command', async () => {
   expect(await vBash()).toStrictEqual(false);
 });
 
+const resGitStatus = {
+  stderr : "",
+  stdout: "On branch main\nYour branch is up to date with 'origin/main'.\n  \nnothing to commit, working tree clean\n"
+};
+
 test('git status', async () => {
-  expect(await vBash("git status")).toStrictEqual({ stdout: 'git_.tests.js\n', stderr: '' });
+  expect(await vBash("git status")).toStrictEqual(resGitStatus);
 });
