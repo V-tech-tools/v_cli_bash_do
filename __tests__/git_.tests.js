c27f3d99c1764472499752c0573e99f40899cc9d
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-03-28 12:35:45 +0200

    yea

--- __tests__/git_.tests.js
+++ __tests__/git_.tests.js
@@ -19,7 +19,7 @@ test('echo something', async () => {
 
 const resGitStatus = {
   stderr: "",
-  stdout: "On branch DEV/fixes001\nYour branch is up to date with 'origin/DEV/fixes001'.\n\nnothing to commit, working tree clean\n"
+  stdout: "On branch DEV/fixes001\nnothing to commit, working tree clean\n"
 };
 
 test('git status', async () => {
