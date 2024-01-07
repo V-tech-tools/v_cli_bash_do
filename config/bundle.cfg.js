7bd7371aee3f0448d04fc2e4a57a9856d5f4a7c2
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2024-01-07 13:22:46 +0100

    yea

---
+++ config/bundle.cfg.js
@@ -0,0 +1,9 @@
+export const isProduction = process.env.NODE_ENV === 'production'
+
+if (isProduction) console.log('✨ Production BUILD')
+
+export const name = 'v_execute'
+export const formats = ['amd', 'cjs', 'es', 'iife', 'umd', 'system']
+
+export const banner = `//! 📚 Package: ${name} \n//! 👨‍💻 Author: V-core9`
+export const footer = `//! - - - - -<[:-v-:]>- - - - - `
---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+import path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
+import strip from '@rollup/plugin-strip'
+
+// Bundle Config Data
+import { isProduction, name, formats, banner, footer } from './config/bundle.cfg.js'
+
+// Rollup Config
+const buildConfig = {
+  input: path.resolve(__dirname, `./source/index.js`),
+  treeshake: 'smallest',
+  output: [
+    // 3 Versions output
+    ...formats.map((format) => ({
+      file: `./dist/${name}.${format}.js`,
+      name,
+      banner,
+      footer,
+      format,
+      sourcemap: isProduction,
+      minifyInternalExports: isProduction,
+      sanitizeFileName: isProduction,
+      generatedCode: {
+        arrowFunctions: true,
+        constBindings: true,
+        conciseMethodProperty: true,
+        objectShorthand: true,
+        parameterDestructuring: true,
+        reservedNamesAsProps: true,
+        stickyRegExp: true,
+        templateString: true
+      }
+      // experimentalMinChunkSize: 1000
+    }))
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    ...(isProduction
+      ? [
+          terser({
+            maxWorkers: 4,
+            compress: {
+              // booleans_as_integers: true,
+              ecma: 2015
+            }
+          }),
+          strip({
+            //labels: ['unittest'],
+            debugger: true
+          })
+        ]
+      : [])
+    // babel({
+    //   exclude: 'node_modules/**'
+    // })
+  ]
+}
+
+export default buildConfig
